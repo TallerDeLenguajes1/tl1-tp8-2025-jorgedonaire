913dec499d2fa618e15a92ed102949677ac1c0fa
Jorge Gregorio Donaire Pereyra <jorgedonaire93@gmail.com>
2025-06-08 23:21:41 -0300

    Terminado el punto 1

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,88 +1,116 @@
 ﻿using espacioTarea;
 
+string cantidadIngresada, opcionIngresada;
+int opcionConvertida;
 List<Tarea> TareasPendientes = new List<Tarea>();
 List<Tarea> TareasRealizadas = new List<Tarea>();
 
-//Carga de 5 tareas aleatorias
-Random rnd = new Random();
-int numeroAleatorio = rnd.Next(10, 100);
-
-for (int i = 0; i < 5; i++)
+Console.WriteLine("*** PROGRAMA DE TAREAS ***");
+Console.WriteLine(" ");
+Console.WriteLine("Ingrese la cantidad de tareas que desee cargar aleatoriamente: ");
+cantidadIngresada = Console.ReadLine();
+if (int.TryParse(cantidadIngresada, out int cantidadConvertida))
 {
-    TareasPendientes.Add(new Tarea(i, $"Tarea nro {i}", numeroAleatorio));
-}
-
-//mostrar lista de pendientes
-Console.WriteLine("***** Lista de TAREAS PENDIENTES *****");
-foreach (Tarea tareaActual in TareasPendientes)
-{
-    Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
-    Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
-    Console.WriteLine($"Duracion: {tareaActual.Duracion}");
-    Console.WriteLine("-----");
-
-}
-Console.WriteLine("*** Fin de lista de tareas pendientes ***");
-
-//mover tareas de pendientes a realizadas
-Console.WriteLine("Ingrese el ID de la tarea que desea marcar como realizada");
-string idIngresado;
-idIngresado = Console.ReadLine();
-int iteracion=0;
-if (int.TryParse(idIngresado, out int idConvertido))
-{
-    foreach (Tarea tareaActual in TareasPendientes)
+    Random rnd = new Random();
+    for (int i = 0; i < cantidadConvertida; i++)
     {
-        if (idConvertido == tareaActual.TareaID)
+        int numeroAleatorio = rnd.Next(10, 100);
+        TareasPendientes.Add(new Tarea(i, $"Descripcion de tarea nro {i}", numeroAleatorio));
+    }
+}
+else
+{
+    Console.WriteLine("Error, debe ingresar un numero");
+}
+
+//INTERFAZ
+do
+{
+    Console.WriteLine(" ");
+    Console.WriteLine("*** MENU ***");
+    Console.WriteLine("Seleccione una opcion: ");
+    Console.WriteLine("Presione 1 para marcar una tarea pendiente como realizada");
+    Console.WriteLine("Presione 2 para buscar una tarea pendiente por su descripcion");
+    Console.WriteLine("Presione 3 para ver las tareas pendientes y realizadas");
+    Console.WriteLine("Presione 4 para salir");
+    Console.WriteLine(" ");
+    opcionIngresada = Console.ReadLine();
+    if (int.TryParse(opcionIngresada, out opcionConvertida))
+    {
+        switch (opcionConvertida)
         {
-            TareasRealizadas.Add(tareaActual);
-            TareasPendientes.RemoveAt(iteracion);
-            break;
+            case 1:
+                Console.WriteLine("Ingrese el ID de la tarea que desea marcar como realizada");
+                string idIngresado;
+                idIngresado = Console.ReadLine();
+                int iteracion = 0;
+                if (int.TryParse(idIngresado, out int idConvertido))
+                {
+                    foreach (Tarea tareaActual in TareasPendientes)
+                    {
+                        if (idConvertido == tareaActual.TareaID)
+                        {
+                            TareasRealizadas.Add(tareaActual);
+                            TareasPendientes.RemoveAt(iteracion);
+                            break;
+                        }
+                        iteracion++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error, ingrese un ID valido");
+                }
+                break;
+            case 2:
+                string descripcionIngresada;
+                Console.WriteLine("Ingrese la descripcion de la tarea buscada: ");
+                descripcionIngresada = Console.ReadLine();
+
+                foreach (Tarea tareaActual in TareasPendientes)
+                {
+                    if (tareaActual.Descripcion == descripcionIngresada)
+                    {
+                        Console.WriteLine("--- Tarea encontrada ---");
+                        Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
+                        Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
+                        Console.WriteLine($"Duracion: {tareaActual.Duracion}");
+                        Console.WriteLine("---");
+                    }
+                }
+                break;
+            case 3:
+                Console.WriteLine("***** Lista de TAREAS PENDIENTES *****");
+                foreach (Tarea tareaActual in TareasPendientes)
+                {
+                    Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
+                    Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
+                    Console.WriteLine($"Duracion: {tareaActual.Duracion}");
+                    Console.WriteLine("-----");
+
+                }
+                Console.WriteLine(" Fin de lista de tareas pendientes ");
+
+                Console.WriteLine("***** Lista de TAREAS REALIZADAS *****");
+                foreach (Tarea tareaActual in TareasRealizadas)
+                {
+                    Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
+                    Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
+                    Console.WriteLine($"Duracion: {tareaActual.Duracion}");
+                    Console.WriteLine("-----");
+
+                }
+                Console.WriteLine(" Fin de lista de tareas realizadas ");
+                break;  
+            case 4:
+                Console.WriteLine("*** FIN DEL PROGRAMA ***");
+                break;
+            default:
+                Console.WriteLine("Ingrese una opcion valida");
+                break;
         }
-        iteracion++;
-    }
-}else
-{
-    Console.WriteLine("Error, ingrese un ID valido");
-}
-
-//mostrar lista de pendientes
-Console.WriteLine("***** Lista de TAREAS PENDIENTES *****");
-foreach (Tarea tareaActual in TareasPendientes)
-{
-    Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
-    Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
-    Console.WriteLine($"Duracion: {tareaActual.Duracion}");
-    Console.WriteLine("-----");
-
-}
-Console.WriteLine(" Fin de lista de tareas pendientes ");
-
-//mostrar lista de realizadas
-Console.WriteLine("***** Lista de TAREAS REALIZADAS *****");
-foreach (Tarea tareaActual in TareasRealizadas)
-{
-    Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
-    Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
-    Console.WriteLine($"Duracion: {tareaActual.Duracion}");
-    Console.WriteLine("-----");
-
-}
-Console.WriteLine(" Fin de lista de tareas realizadas ");
-
-//busqueda de tareas por descripcion
-string descripcionIngresada;
-Console.WriteLine("Ingrese la descripcion de la tarea buscada: ");
-descripcionIngresada = Console.ReadLine();
-
-foreach (Tarea tareaActual in TareasPendientes)
-{
-    if (tareaActual.Descripcion == descripcionIngresada)
+    }else
     {
-        Console.WriteLine("--- Tarea encontrada ---");
-        Console.WriteLine($"Tarea ID: {tareaActual.TareaID}");
-        Console.WriteLine($"Descripcion: {tareaActual.Descripcion}");
-        Console.WriteLine($"Duracion: {tareaActual.Duracion}");
-        Console.WriteLine("---");
+        Console.WriteLine("Error, debe ingresar un numero");   
     }
-}
+} while (opcionConvertida != 4);
